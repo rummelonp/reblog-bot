7f106e58b6904ca06f554126c41bba411d74e598
Kazuya Takeshima <mail@mitukiii.jp>
2013-04-12 01:11:00 +0900

    Fix schedule.rb

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,24 +1,22 @@
-# -*- coding: undecided -*-
+# -*- coding: utf-8 -*-
 
-$:.unshift File.dirname(__FILE__) + '/../lib'
+root = File.dirname(__FILE__) + '/..'
+$:.unshift File.join(root, 'lib')
 
-require 'rubygems'
-require 'bundler'
-Bundler.require
 require 'reblog_bot'
 
 job_type :reblog_bot, 'cd :path && ./reblog_bot.rb :task :output'
 
-@config = ReblogBot::ConfigLoader.load
-@config.accounts.each do |name, account|
+@config = ReblogBot::Environment.instance.config
+@config[:accounts].each do |name, account|
   log_name = "log/#{name}.log"
 
   every 12.hours do
-    reblog_bot "followback #{name}", output: log_name
+    reblog_bot "followback #{name}", :output => log_name
   end
 
-  next unless account.every
-  every instance_eval(account.every) do
-    reblog_bot "reblog #{name}", output: log_name
+  next unless account[:every]
+  every instance_eval(account[:every]) do
+    reblog_bot "reblog #{name}", :output => log_name
   end
 end
